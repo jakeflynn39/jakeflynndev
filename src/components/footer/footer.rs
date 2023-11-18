f1ed8d652beea862c9a66288b5690e178c76b29b
JAke Flynn <jpflynn01@gmail.com>
2023-11-18 12:02:03 -0800

    added footer, but need linkedin logo

--- src/components/footer/footer.rs
+++ src/components/footer/footer.rs
@@ -4,7 +4,18 @@ use leptos::*;
 pub fn Footer() -> impl IntoView {
     view! {
         <footer>
-            // <h1>{ "Footer" }</h1>
+            <div class="footer-link">
+                <div class="icon">
+                    <a href="https://github.com/jakeflynn39" class="gh" target="_blank">
+                        <img src="/static/images/github-mark-white.svg" alt="Github icon" />
+                    </a>
+                </div>
+                <div class="icon">
+                    <a href="https://github.com/jakeflynn39" class="gh" target="_blank">
+                        <img src="/static/images/github-mark-white.svg" alt="Github icon" />
+                    </a>
+                </div>
+            </div>
         </footer>
     }
 }
